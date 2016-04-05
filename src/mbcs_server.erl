ac72d9d7b5ac3d1d92c18f1ffd45611251baa69d
dudgh <y@xenix.net>
2016-04-05 22:52:17 +0900

    modify dict, set type erlang 18

--- src/mbcs_server.erl
+++ src/mbcs_server.erl
@@ -24,15 +24,15 @@
 %%--------------------------------------------------------------------------
          
 -record(mbcs_codecs_item, {
-	  undefined    :: set(),	% undefine bytes
-	  leadbytes	   :: set(),    % dbcs lead bytes
-	  mbtable      :: dict(),	% multiple byte to wide character table
-	  wctable      :: dict()    % wide character to multiple byte table
+	  undefined    :: sets:set(),	% undefine bytes
+	  leadbytes	   :: sets:set(),    % dbcs lead bytes
+	  mbtable      :: dict:dict(),	% multiple byte to wide character table
+	  wctable      :: dict:dict()    % wide character to multiple byte table
 	 }).
 
 -record(mbcs_server, {
-	  codecs       :: dict(),	% codecs name to codecs type 'unicode' | 'mbcs' | 'gb18030'
-	  mbcs_codecs  :: dict()    % mbcs codecs name to #mbcs_codecs_item{}
+	  codecs       :: dict:dict(),	% codecs name to codecs type 'unicode' | 'mbcs' | 'gb18030'
+	  mbcs_codecs  :: dict:dict()    % mbcs codecs name to #mbcs_codecs_item{}
 	 }).
      
 -record(mbcs_options, {
